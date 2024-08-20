413e9733474109038cd20de0914d289749ec39b0
Afonso Gesualdi <152401918+gesualdiafonso@users.noreply.github.com>
2024-08-20 12:31:36 -0300

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const track = document.querySelector('.carousel-track');
+    const slides = Array.from(track.children);
+    const nextButton = document.querySelector('.carousel-button.next');
+    const prevButton = document.querySelector('.carousel-button.prev');
+    let slideWidth = slides[0].getBoundingClientRect().width;
+
+    // Ajustar a largura mínima dos slides
+    if (slideWidth < 1200) {
+        slideWidth = 1200;
+    }
+
+    // Função para definir a posição de cada slide
+    const setSlidePosition = (slide, index) => {
+        slide.style.left = slideWidth * index + 'px';
+    };
+    slides.forEach(setSlidePosition);
+
+    const moveToSlide = (track, currentSlide, targetSlide) => {
+        track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+        currentSlide.classList.remove('current-slide');
+        targetSlide.classList.add('current-slide');
+    };
+
+    // Quando clicar no botão de anterior
+    prevButton.addEventListener('click', e => {
+        const currentSlide = track.querySelector('.current-slide');
+        const prevSlide = currentSlide.previousElementSibling;
+
+        if (prevSlide) {
+            moveToSlide(track, currentSlide, prevSlide);
+        }
+    });
+    
+    // Quando clicar no botão de próximo
+    nextButton.addEventListener('click', e => {
+        const currentSlide = track.querySelector('.current-slide');
+        const nextSlide = currentSlide.nextElementSibling;
+
+        if (nextSlide) {
+            moveToSlide(track, currentSlide, nextSlide);
+        }
+    });
+
+    // Iniciando o primeiro slide
+    slides[0].classList.add('current-slide');
+});
